3b041db6556905bb77ab483e5897d947def217e6
Cody Kremer <ckremer99@outlook.com>
2024-08-09 10:21:07 -0700

    made user model

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-// const User = require("../models/user");
+const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 const SALT_LENGTH = 12;
